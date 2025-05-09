c56cbea30e84a26d2dc7f540f1886367b1e86716
Gaolingx <947770192@qq.com>
2025-05-10 02:14:45 +0800

    2025051001

---
+++ Project/汉诺塔/v1/G2-汉诺塔/G2-汉诺塔/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace G2_汉诺塔
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(117, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A柱";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(146, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(19, 659);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(592, 125);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(19, 659);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(563, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B柱";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(1038, 125);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(19, 659);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(1009, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 35);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "C柱";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 839);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Project/汉诺塔/v1/G2-汉诺塔/G2-汉诺塔/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G2_汉诺塔
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitGame(6);
+        }
+        bool isDown=false;
+        Point old;
+        private void pb_MouseDown(object sender, MouseEventArgs e)
+        {
+            PictureBox pb=sender as PictureBox;
+            if (listA[listA.Count - 1].Equals(pb))
+            {
+                isDown = true;
+                old = pb.Location;
+            }
+            
+        }
+
+        private void pb_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown)
+            {
+                PictureBox pb=sender as PictureBox;
+                //MousePosition 屏幕的鼠标位置
+                int x = MousePosition.X-this.Location.X-pb.Width/2;
+                int y = MousePosition.Y - this.Location.Y -pb.Height/2;
+                Point p=new Point(x, y);
+                pb.Location = p;
+            }
+        }
+
+        private void pb_MouseUp(object sender, MouseEventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            //2个矩形是否相交
+            if (pictureBox2.Bounds.IntersectsWith(pb.Bounds))
+            {
+                SetPic(pb,pictureBox2);
+            }
+            isDown = false;
+            
+        }
+
+        private void SetPic(PictureBox pb, PictureBox pictureBox2)
+        {
+            List<PictureBox> list =(List<PictureBox>) pictureBox2.Tag;
+            int x = pictureBox2.Location.X + pictureBox2.Width / 2-pb.Width/2;
+            int y = pictureBox2.Location.Y + pictureBox2.Height-pb.Height;
+            if (list.Count == 0)
+            {
+                pb.Location=new Point(x, y);
+                list.Add(pb);
+                List<PictureBox> listTem = (List<PictureBox>)pb.Tag;
+                listTem.Remove(pb);
+                pb.Tag = list;
+                return;
+            }
+            if (list[list.Count - 1].Width > pb.Width)
+            {
+                pb.Location = new Point(x, y-list.Count*60);
+                list.Add(pb);
+                List<PictureBox> listTem = (List<PictureBox>)pb.Tag;
+                listTem.Remove(pb);
+                pb.Tag = list;
+            }
+            else
+            {
+                pb.Location = old;
+            }
+        }
+
+        List<PictureBox> listA= new List<PictureBox>();
+        List<PictureBox> listB = new List<PictureBox>();
+        List<PictureBox> listC = new List<PictureBox>();
+        void InitGame(int n)
+        {
+            int x=pictureBox1.Location.X+pictureBox1.Width/2;
+            int y=pictureBox1.Location.Y+pictureBox1.Height;
+            for (int i = n; i>0; i--)
+            {
+                PictureBox pb = new System.Windows.Forms.PictureBox();
+
+                pb.BackColor = System.Drawing.Color.Blue;
+               
+                pb.Name = "pb";
+                pb.Size = new System.Drawing.Size(100+i*20, 50);
+                pb.Location = new System.Drawing.Point(x-pb.Width/2, y -pb.Height- (n-i) * 60);
+                pb.TabIndex = 6;
+                pb.TabStop = false;
+                pb.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pb_MouseDown);
+                pb.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb_MouseMove);
+                pb.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pb_MouseUp);
+
+                this.Controls.Add(pb);
+                pb.BringToFront();
+                listA.Add(pb);
+                pb.Tag = listA;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Tag = listA;
+            pictureBox2.Tag = listB;
+            pictureBox3.Tag = listC;
+        }
+    }
+}
